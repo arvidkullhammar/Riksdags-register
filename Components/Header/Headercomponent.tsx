451bdc2d874e3a6da6dd985217b57e43da1f60e0
Arvid Kullhammar <91427561+arvidkullhammar@users.noreply.github.com>
2022-11-04 14:26:59 +0100

    updated header component

--- Components/Header/Headercomponent.tsx
+++ Components/Header/Headercomponent.tsx
@@ -9,62 +9,65 @@ import React, { useState } from "react";
 import styles from "./Header.module.css";
 
 export default function Membercard(props: any) {
-  const [age, setAge] = useState("");
-  const [party, setParty] = useState<any | null>(null);
+  const { ageCallback, partyCallback, age, party } = props;
 
-  const partyChange = (event: SelectChangeEvent) => {
-    setParty(event.target.value as string);
+  const partyChange = (event: any) => {
+    console.log(event);
+    partyCallback(event);
   };
-  const ageChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value as string);
+  const ageChange = (event: any) => {
+    console.log(event);
+    ageCallback(event);
   };
 
   return (
-    <div className={styles.container}>
-      <FormControl
-        variant="standard"
-        sx={{ m: 1, minWidth: 120, maxWidth: 400 }}
-      >
-        <InputLabel id="age-label">Ålder</InputLabel>
-        <Select
-          labelId="age-label"
-          id="age-select"
-          value={age}
-          label="age"
-          onChange={ageChange}
+    <div className={styles.maincontainer}>
+      <div className={styles.container}>
+        <FormControl
+          variant="standard"
+          sx={{ m: 1, minWidth: 150, maxWidth: 400 }}
         >
-          <MenuItem value="DEFAULT" disabled>
-            None
-          </MenuItem>
-          <MenuItem value="ascending">Ascending</MenuItem>
-          <MenuItem value="descending">Descending</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl
-        variant="standard"
-        sx={{ m: 1, minWidth: 120, maxWidth: 400 }}
-      >
-        <InputLabel id="party-label">Parti</InputLabel>
-        <Select
-          labelId="party-label"
-          id="party-select"
-          value={party}
-          label="party"
-          onChange={partyChange}
+          <InputLabel id="age-label">Ålder</InputLabel>
+          <Select
+            labelId="age-label"
+            id="age-select"
+            value={age}
+            label="age"
+            onChange={(e) => ageChange(e?.target.value)}
+          >
+            <MenuItem value="DEFAULT" disabled>
+              None
+            </MenuItem>
+            <MenuItem value="ascending">Ascending</MenuItem>
+            <MenuItem value="descending">Descending</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl
+          variant="standard"
+          sx={{ m: 1, minWidth: 150, maxWidth: 400 }}
         >
-          <MenuItem value="DEFAULT" disabled>
-            None
-          </MenuItem>
-          <MenuItem value="C">Centerpartiet</MenuItem>
-          <MenuItem value="L">Liberalerna</MenuItem>
-          <MenuItem value="MP">Miljöpartiet</MenuItem>
-          <MenuItem value="M">Moderaterna</MenuItem>
-          <MenuItem value="S">Socialdemokraterna</MenuItem>
-          <MenuItem value="SD">Sverigedemokraterna</MenuItem>
-          <MenuItem value="KD">Kristdemokraterna</MenuItem>
-          <MenuItem value="V">Vänsterpartiet</MenuItem>
-        </Select>
-      </FormControl>
+          <InputLabel id="party-label">Parti</InputLabel>
+          <Select
+            labelId="party-label"
+            id="party-select"
+            value={party}
+            label="party"
+            onChange={(e) => partyChange(e?.target.value)}
+          >
+            <MenuItem value="DEFAULT" disabled>
+              None
+            </MenuItem>
+            <MenuItem value="C">Centerpartiet</MenuItem>
+            <MenuItem value="L">Liberalerna</MenuItem>
+            <MenuItem value="MP">Miljöpartiet</MenuItem>
+            <MenuItem value="M">Moderaterna</MenuItem>
+            <MenuItem value="S">Socialdemokraterna</MenuItem>
+            <MenuItem value="SD">Sverigedemokraterna</MenuItem>
+            <MenuItem value="KD">Kristdemokraterna</MenuItem>
+            <MenuItem value="V">Vänsterpartiet</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import styles from "../styles/Home.module.css";
 const axios = require("axios");
 import { useEffect, useState } from "react";
 import Membercard from "../Components/Membercard/Membercard";
+import Headercomponent from "../Components/Header/Headercomponent";
 
 export default function Home() {
   const [list, setList] = useState([] as any[]);
@@ -49,45 +50,25 @@ export default function Home() {
 
   return (
     <div className={styles.maincontainer}>
-      <div className={styles.searchbar}>
-        <div>
-          Ålder:
-          <select
-            defaultValue={"DEFAULT"}
-            onChange={(e) => sortAge(e.target.value)}
-          >
-            <option value="DEFAULT" disabled>
-              None
-            </option>
-            <option value="ascending">Ascending</option>
-            <option value="descending">Descending</option>
-          </select>
-        </div>
-        <div>
-          Parti:
-          <select
-            defaultValue={"DEFAULT"}
-            onChange={(e) => sortParty(e.target.value)}
-          >
-            <option value="DEFAULT" disabled>
-              None
-            </option>
-            <option value="C">Centerpartiet</option>
-            <option value="L">Liberalerna</option>
-            <option value="MP">Miljöpartiet</option>
-            <option value="M">Moderaterna</option>
-            <option value="S">Socialdemokraterna</option>
-            <option value="SD">Sverigedemokraterna</option>
-            <option value="KD">Kristdemokraterna</option>
-            <option value="V">Vänsterpartiet</option>
-          </select>
-        </div>
+      <div className={styles.header}>
+        <Headercomponent
+          age={age}
+          party={party}
+          ageCallback={sortAge}
+          partyCallback={sortParty}
+        />
       </div>
 
       {getSortedList() ? (
         <div className={styles.container}>
           {getSortedList().map((member: any, i: any) => {
-            return <Membercard key={member.sourceid} data={member} />;
+            return (
+              <Membercard
+                className={styles.card}
+                key={member.sourceid}
+                data={member}
+              />
+            );
           })}
         </div>
       ) : (
